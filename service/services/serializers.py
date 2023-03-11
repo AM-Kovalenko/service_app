d20a34d147583aee3f5f32126b153a405e59a3e5
Alexander Kovalenko <alex.kovalenko@skz.by>
2023-03-11 21:41:43 +0300

    day-3-1

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -1,12 +1,18 @@
 from rest_framework import serializers
 
-from services.models import Subscriptions
+from services.models import Subscriptions, Plan
 
+
+class PlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plan
+        fields = ('__all__')
 
 class SubscriptionSerializer(serializers.ModelSerializer):
+    plan = PlanSerializer() # вложенный сериалайзер
     client_name = serializers.CharField(source='client.company_name')
     email = serializers.CharField(source='client.user.email')
 
     class Meta:
         model = Subscriptions
-        fields = ('id', 'plan_id', 'client_name', 'email')
+        fields = ('id', 'plan_id', 'client_name', 'email', 'plan')
--- service/services/views.py
+++ service/services/views.py
@@ -12,6 +12,7 @@ class SubscriptionsView(ReadOnlyModelViewSet):
     # queryset = Subscriptions.objects.all().prefetch_related('client').prefetch_related('client__user')
 
     queryset = Subscriptions.objects.all().prefetch_related(
+        'plan',
         Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name',
                                                                                      'user__email'))
 
