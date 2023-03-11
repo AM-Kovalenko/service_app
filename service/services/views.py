b646bceece82ae2cc56127596d9b5c751a2b63a2
Alexander Kovalenko <alex.kovalenko@skz.by>
2023-03-11 21:18:00 +0300

    day-3-1

--- service/services/views.py
+++ service/services/views.py
@@ -1,3 +1,19 @@
+from django.db.models import Prefetch
 from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
-# Create your views here.
+from clients.models import Client
+from services.models import Subscriptions
+from services.serializers import SubscriptionSerializer
+
+
+class SubscriptionsView(ReadOnlyModelViewSet):
+    # оптимизация квери запроса. убираем проблему n+1. bcgjkmpetv prefetch_related, чтоб не делать доп запросы
+    # queryset = Subscriptions.objects.all().prefetch_related('client').prefetch_related('client__user')
+
+    queryset = Subscriptions.objects.all().prefetch_related(
+        Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name',
+                                                                                     'user__email'))
+
+    )
+    serializer_class = SubscriptionSerializer
